0c8f2905d66c4043b57db5add02f6feedd1ca1a4
kishan955971 <kishan221314z@gmail.com>
2025-08-02 01:40:35 +0530

    lenght() java

--- in.java
+++ in.java
@@ -1,12 +1,9 @@
 
-import java.util.*;
 public class in {
 
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        System.out.print("Enter Your Name : ");
-        String name=sc.nextLine();
-        System.out.println(name);
+        String name="hellow";
+        System.out.println(name.length());
 
     }
     
