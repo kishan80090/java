e61c5d5f74672157a385a0f6b10f50e7ccd982e8
kishan955971 <kishan221314z@gmail.com>
2025-08-02 01:29:08 +0530

    hellow java

--- InputString.java
+++ InputString.java
@@ -1,13 +1,9 @@
 
 public class InputString {
 
-
 public static void main(String[] args) {
-int arry[]={1,2,3,4,5,6};
-for(int i=0;i<=arry.length-1;i++)
-{
-    System.out.println(arry[i]);
-}
+
+    System.out.println("hellow word");
     
 }
 }
