697534c44cf2c3d55fce6e8a5e5b710a43250969
kishan955971 <kishan221314z@gmail.com>
2025-08-02 01:38:46 +0530

    input

---
+++ evenodd.java
@@ -0,0 +1,11 @@
+public class evenodd {
+    public static void main(String[] args){
+        int num=7;
+        if(num%2==0)
+        {
+            System.out.println("number is even");
+        }else{
+            System.out.println("number is odd");
+        }
+    }
+}
